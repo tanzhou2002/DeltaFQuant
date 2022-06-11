b46428bca28ad42f6ca1ab0dd42d68d1472c9af2
tanzhou <3237510657@qq.com>
2022-06-11 13:46:52 +0800

    数据服务端有问题

---
+++ Data/Stock.py
@@ -0,0 +1,8 @@
+from jqdatasdk import *
+
+auth('18756241608', 'Tanzhou666')
+
+# # 获取平安银行2022-05-30为截至, 以daily为频率, 获取100条数据
+# df = get_price('000001.XSHE', end_date='2022-05-30', count=100, frequency='daily',
+#                fields=['open', 'close', 'high', 'low', 'volume', 'money'])
+# print(df)
